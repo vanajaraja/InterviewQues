e2ee3203df03509c0374f0351b6fa44294ee7514
vanaja <vanajasadasivam@gmail.com>
2022-12-16 13:01:21 +0800

    Added new Class file

---
+++ src/test/java/org/test/qa/SampleTestClass.java
@@ -0,0 +1,12 @@
+package org.test.qa;
+
+import java.util.Date;
+
+public class SampleTestClass {
+	
+	private void countDown() {
+		// TODO Auto-generated method stub
+		System.out.println(new Date());
+	}
+
+}
